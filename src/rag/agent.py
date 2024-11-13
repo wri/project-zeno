d601463dde2c5a72f5891b62abf58f20846b322c
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2024-11-13 16:08:31 +0000

    RAG Agent setup

---
+++ src/rag/agent.py
@@ -0,0 +1,44 @@
+from langgraph.graph import END, START, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from rag.utils.nodes import agent, generate, grade_documents, rewrite
+from rag.utils.state import AgentState
+from rag.utils.tools import retriever_tool
+
+# Define a new graph
+workflow = StateGraph(AgentState)
+
+# Define the nodes we will cycle between
+workflow.add_node("agent", agent)  # agent
+retrieve = ToolNode([retriever_tool])
+workflow.add_node("retrieve", retrieve)  # retrieval
+workflow.add_node("rewrite", rewrite)  # Re-writing the question
+workflow.add_node(
+    "generate", generate
+)  # Generating a response after we know the documents are relevant
+# Call agent node to decide to retrieve or not
+workflow.add_edge(START, "agent")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "agent",
+    # Assess agent decision
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+
+# Edges taken after the `action` node is called.
+workflow.add_conditional_edges(
+    "retrieve",
+    # Assess agent decision
+    grade_documents,
+)
+workflow.add_edge("generate", END)
+workflow.add_edge("rewrite", "agent")
+
+# Compile
+graph = workflow.compile()
---
+++ src/rag/utils/chunk.py
@@ -0,0 +1,52 @@
+"""
+Module to do chunking of GFW blog posts and create a Chroma vector
+database to support a simple RAG agent.
+"""
+
+import unicodedata
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from langchain_chroma.vectorstores import Chroma
+from langchain_community.document_loaders import CSVLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# Open blog posts from WordPress export
+with open("data/gfw-blog-and-help-center-posts.xml") as fl:
+    soup = BeautifulSoup(fl, "xml")
+
+# Parse xml and extract blog content as text
+bodies = []
+titles = []
+for item in soup.channel.find_all("item"):
+
+    html = item.find_all("content:encoded")[0]
+    sopa = BeautifulSoup(html.contents[0], "html.parser").get_text()
+    txt = unicodedata.normalize("NFKC", sopa)
+    txt = txt.replace("\n", "")
+    bodies.append(txt)
+
+    title = item.title.contents[0]
+    titles.append(title)
+
+# Persist resulting data as csv
+df = pd.DataFrame({"title": titles, "body": bodies})
+df.to_csv("data/gfw-blog.csv", index=False, columns=["title", "body"])
+
+# Load data from csv
+loader = CSVLoader(
+    file_path="data/gfw-blog.csv",
+    source_column="title",
+)
+docs = loader.load()
+
+# Split into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+
+vectorstore = Chroma.from_documents(
+    documents=splits,
+    embedding=OllamaEmbeddings(model="nomic-embed-text"),
+    persist_directory="data/chroma_gfw",
+)
