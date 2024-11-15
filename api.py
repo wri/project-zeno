c8d93ab08d102236658f3d2f5075ff833c576ee4
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2024-11-15 15:23:30 +0000

    Made streamlit app and streaming json data

--- api.py
+++ api.py
@@ -1,29 +1,28 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Body
-from fastapi.responses import StreamingResponse
+from fastapi.responses import Response, StreamingResponse, JSONResponse
 from langchain_core.messages import HumanMessage, ToolMessage
 from rag.agent import graph
-
+import json
 
 app = FastAPI()
 
 # https://www.softgrade.org/sse-with-fastapi-react-langgraph/
-
+# https://www.workfall.com/learning/blog/how-to-stream-json-data-using-server-sent-events-and-fastapi-in-python-over-http/
 
 def event_stream(query: str):
+
     initial_state = {"messages": [HumanMessage(content=query)]}
+
     for output in graph.stream(initial_state):
-        print(output)
         for node_name, node_results in output.items():
-            yield f"---Output from {node_name}---"
-            chunk_messages = node_results.get("messages", [])
-            for message in chunk_messages:
+            for message in node_results.get("messages", []):
                 if hasattr(message, "content"):
-                    print(message.content)
-                    yield message.content
+                    # result[node_name] = message.content
+                    yield json.dumps({node_name: message.content}) + "\n"
 
 
 @app.post("/stream")
 async def stream(query: Annotated[str, Body(embed=True)]):
-    return StreamingResponse(event_stream(query), media_type="text/event-stream")
+    return StreamingResponse(event_stream(query), media_type="application/x-ndjson")
