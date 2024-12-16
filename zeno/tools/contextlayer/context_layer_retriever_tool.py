0e587a1f2ce307193630379caec1bfafbec049da
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2024-12-16 15:57:57 +0000

    Intermediate step in making the agent work

--- zeno/tools/contextlayer/context_layer_retriever_tool.py
+++ zeno/tools/contextlayer/context_layer_retriever_tool.py
@@ -1,18 +1,23 @@
-import json
 import os
 
+import lancedb
+from langchain_community.vectorstores import LanceDB
 from langchain_core.tools import tool
 from langchain_core.tools.retriever import create_retriever_tool
-from langchain_community.vectorstores import LanceDB
 from langchain_ollama import OllamaEmbeddings
+from langchain_ollama.embeddings import OllamaEmbeddings
 from pydantic import BaseModel, Field
 
 from zeno.agents.maingraph.models import ModelFactory
-from zeno.tools.contextlayer.layers import DatasetNames, layer_choices
+from zeno.tools.contextlayer.layers import DatasetNames
+
+embedder = OllamaEmbeddings(model="nomic-embed-text")
+table = lancedb.connect("data/layers-context").open_table("zeno-layers-context")
+
 
 # TODO: add reranker?
 vectorstore = LanceDB(
-    uri="s3://zeno-static-data/layers-context",
+    uri="data/layers-context",
     region="us-east-1",
     embedding=OllamaEmbeddings(
         model="nomic-embed-text", base_url=os.environ["OLLAMA_BASE_URL"]
@@ -29,43 +34,33 @@ retriever_tool = create_retriever_tool(
 )
 
 
-# class grade(BaseModel):
-#     """Choice of landcover."""
+class grade(BaseModel):
+    """Choice of landcover."""
 
-#     choice: DatasetNames = Field(description="Choice of context layer to use")
-
-
-# class ContextLayerInput(BaseModel):
-#     """Input schema for context layer tool"""
-
-#     question: str = Field(description="The question from the user")
+    choice: DatasetNames = Field(description="Choice of context layer to use")
 
 
-# model = ModelFactory().get("claude-3-5-sonnet-latest").with_structured_output(grade)
+class ContextLayerInput(BaseModel):
+    """Input schema for context layer tool"""
+
+    question: str = Field(description="The question from the user")
 
 
-# @tool("context-layer-tool", args_schema=ContextLayerInput, return_direct=False)
-# def context_layer_tool(question: str) -> DatasetNames:
-#     """
-#     Determines whether the question asks for summarizing by land cover.
-#     """
+model = ModelFactory().get("claude-3-5-sonnet-latest").with_structured_output(grade)
 
-#     print("---CHECK CONTEXT LAYER TOOL---")
 
-#     query = (
-#         f"""You are a deciding if a context layer is required for analysing disturbance alerts. \n
-#     Here is the user question: {question} \n
-#     If the question does not ask for grouping, return empty string. \n
-#     If the question asks for grouping the disturbance alerts by landcover, decide which landcover layer is most appropriate. \n
+@tool("context-layer-tool", args_schema=ContextLayerInput, return_direct=False)
+def context_layer_tool(question: str) -> DatasetNames:
+    """
+    Determines whether the question asks for summarizing by land cover.
+    """
 
-#     The following json data gives information about the available layers. Pick the most appropriate one and return its 'dataset' value.
-#     Never change the returned 'dataset' value, always return it as is. \n
-#     """
-#         + json.dumps(layer_choices),
-#     )
+    print("---CHECK CONTEXT LAYER TOOL---")
+    embedding = embedder.embed_query(question)
 
-#     result = model.invoke(query)
+    matches = table.search(embedding).limit(5).to_pandas()
 
-#     print(f"---DECISION: {result.choice or 'no landcover needed'}---")
+    # TODO: add a filter that returns most recent by default.
 
-#     return result.choice
+    # return matches.dataset.value
+    return matches.dataset.values[0]
