b2193fa700b3ff382d58067c3d27ed1e31080e91
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-02-13 09:04:02 +0000

    Fix chroma layers path

--- zeno/agents/layerfinder/tool_layer_retrieve.py
+++ zeno/agents/layerfinder/tool_layer_retrieve.py
@@ -7,7 +7,7 @@ embedder = OllamaEmbeddings(
     model="nomic-embed-text", base_url=os.environ["OLLAMA_BASE_URL"]
 )
 db = Chroma(
-    persist_directory="data/chroma_layers_bla",
+    persist_directory="data/chroma_layers",
     embedding_function=embedder,
     create_collection_if_not_exists=False,
 )
