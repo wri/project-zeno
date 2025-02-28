09a2aced8696054241bbda9406d39bf3732902ab
srmsoumya <vnn.soumya2@gmail.com>
2025-02-28 12:50:24 +0530

    Add support for KBA name search

--- zeno/agents/kba/tool_kba_data.py
+++ zeno/agents/kba/tool_kba_data.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from typing import Annotated
+from typing import Annotated, Optional, List
 
+import pandas as pd
 import geopandas as gpd
 from langchain_core.messages import ToolMessage
 from langchain_core.tools import tool
 from langchain_core.tools.base import InjectedToolCallId
 from langgraph.types import Command
+from thefuzz import process
 
 data_dir = Path("data/kba")
 kba = gpd.read_file(data_dir / "kba_merged.gpkg")
@@ -26,25 +28,35 @@ def get_aoi(gadm_id: str, gadm_level: int, buffer_distance: float = 0.1):
 
 @tool("kba-data-tool")
 def kba_data_tool(
-    name: str,
-    gadm_id: str,
-    gadm_level: int,
-    tool_call_id: Annotated[str, InjectedToolCallId],
+    name: Optional[str] = None,
+    gadm_id: Optional[str] = None,
+    gadm_level: Optional[int] = None,
+    kba_names: Optional[List[str]] = None,
+    tool_call_id: Annotated[str, InjectedToolCallId] = None,
 ) -> Command:
     """
     Finds data for Key Biodiversity Areas (KBAs) within an area of interest.
 
     Args:
-        name: Name of the location to search for. Can be a city, region, or country name.
-        gadm_id: GADM ID of the location to search for.
-        gadm_level: GADM level of the location to search for.
+        name: Name of the location to search for (optional).
+        gadm_id: GADM ID of the location to search for (optional).
+        gadm_level: GADM level of the location to search for (optional).
+        kba_names: List of KBAs to search for (optional). If provided, only data for these KBAs will be returned.
     """
     print("kba data tool")
-    aoi = get_aoi(gadm_id=gadm_id, gadm_level=gadm_level)
 
-    kba_within_aoi = kba[kba.geometry.within(aoi)]
-
-    data = f"Found data for {len(kba_within_aoi)} KBAs within the area of interest: {name}."
+    if kba_names:
+        # fuzzy match over kba names
+        result = []
+        for kba_name in kba_names:
+            *_, index = process.extractOne(kba_name, kba.siteName)
+            result.append(kba.iloc[index])
+        kba_within_aoi = gpd.GeoDataFrame(result, geometry="geometry")
+        data = f"Found KBAs: {kba_within_aoi.siteName.to_list()}"
+    else:
+        aoi = get_aoi(gadm_id=gadm_id, gadm_level=gadm_level)
+        kba_within_aoi = kba[kba.geometry.within(aoi)]
+        data = f"Found data for {len(kba_within_aoi)} KBAs within the area of interest: {name}."
     return Command(
         update={
             "kba_within_aoi": kba_within_aoi.to_json(),
