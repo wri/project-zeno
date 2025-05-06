149bffadbb4d58500b13accc7eb862c4d6d08150
Sanjay Bhangar <sanjay@developmentseed.org>
2025-05-06 15:12:57 +0530

    dont error if database already exists

--- db/init-multiple-dbs.sh
+++ db/init-multiple-dbs.sh
@@ -6,6 +6,14 @@ export PGPASSWORD="${POSTGRES_PASSWORD}"
 
 # Create additional databases if they do not exist
 psql -h "${POSTGRES_HOST}" -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE "${APP_DB:-zeno-data}";
-    CREATE DATABASE "${LANGFUSE_DB:-langfuse}";
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${APP_DB:-zeno-data}') THEN
+            CREATE DATABASE "${APP_DB:-zeno-data}";
+        END IF;
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${LANGFUSE_DB:-langfuse}') THEN
+            CREATE DATABASE "${LANGFUSE_DB:-langfuse}";
+        END IF;
+    END
+    \$\$;
 EOSQL
