cd6c93dbc62765203e45d08e372a7b950f05c527
Sanjay Bhangar <sanjay@developmentseed.org>
2025-05-06 15:05:28 +0530

    use pg host and pg password in init-multiple-dbs script

--- db/init-multiple-dbs.sh
+++ db/init-multiple-dbs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Set password for psql
+export PGPASSWORD="${POSTGRES_PASSWORD}"
+
 # Create additional databases if they do not exist
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -h "${POSTGRES_HOST}" -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE "${APP_DB:-zeno-data}";
     CREATE DATABASE "${LANGFUSE_DB:-langfuse}";
 EOSQL
