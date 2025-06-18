061f05965e24cc02cd59e33f3388eddb448b6e54
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-06-18 09:42:31 +0100

    Remove dist alerts tool

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,16 +1,12 @@
-from .context_layer import context_layer_tool
-from .dist_alerts import dist_alerts_tool
+from .dataset_finder import dataset_finder_tool
+from .gfw_query import gfw_query_tool
 from .kba_data import kba_data_tool
 from .kba_insights import kba_insights_tool
 from .kba_timeseries import kba_timeseries_tool
 from .location import location_tool
 from .stac import stac_tool
-from .gfw_query import gfw_query_tool
-from .dataset_finder import dataset_finder_tool
 
 __all__ = [
-    "context_layer_tool",
-    "dist_alerts_tool",
     "kba_data_tool",
     "kba_insights_tool",
     "kba_timeseries_tool",
