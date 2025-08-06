ab8c1c5bbcd889b78eb5ba8a136177b8e79b4bf6
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-08-06 19:14:14 +0100

    Polish embed script

--- src/ingest/embed_datasets.py
+++ src/ingest/embed_datasets.py
@@ -20,7 +20,7 @@ data_dir = Path("data").absolute()
 
 print(f"Loading zeno data from {data_dir}")
 
-zeno_data = pd.read_csv(data_dir / "zeno_data_clean.csv")
+zeno_data = pd.read_csv(data_dir / "zeno_data_clean_v2.csv")
 
 openai_embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
@@ -75,27 +75,4 @@ for idx, row in zeno_data.iterrows():
 
 openai_index.add_documents(documents=zeno_docs)
 
-# Save & load_again
 openai_index.dump(data_dir / "zeno-docs-openai-index")
-
-# openai_index = InMemoryVectorStore.load(data_dir / "zeno-docs-openai-index", embedding=openai_embeddings)
-
-# queries = [
-#     "What percent of 2000 forest did Kalimantan Barat lose from 2001 through 2024?",
-#     "What percent of Johor was tree cover in 2000?",
-#     "Which year recorded more alerts within Protected Areas in Ucayali, Peru? 2023 or 2024?",
-#     "Since 2001, do the forests within Gomba's KBAs act as a net carbon source or a net carbon sink, and by how much?",
-#     "Has Chai Nat or Krabi in Thailand has had the most forest-related annual carbon emissions since 2001?"
-#     "I'm researching carbon sequestration in Indonesian forests. Specifically, for Key Biodiversity Areas (KBAs), how much carbon has been absorbed from the atmosphere between 2000 and 2024?",
-#     "How many deforestation alerts were reported in protected areas of the Republic of the Congo april 2024 - april 2025?",
-#     "which country had the most deforestation in 2018"
-# ]
-
-# openai_retriever = openai_index.as_retriever(
-#     search_type="similarity",
-#     search_kwargs={"k": 3},
-# )
-
-# r = openai_retriever.batch(queries)
-
-# print(r)
