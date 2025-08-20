964d82d4fc8c01eb4243afad0d6d00abe75430e0
Sanjay Bhangar <sanjay@developmentseed.org>
2025-08-20 11:32:01 +0530

    fix created_at and updated_at timestamps

--- src/api/data_models.py
+++ src/api/data_models.py
@@ -32,8 +32,8 @@ class UserOrm(Base):
     id = Column(String, primary_key=True, unique=True, nullable=False)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
-    updated_at = Column(DateTime, nullable=False, default=datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(DateTime, nullable=False, default=datetime.now)
     user_type = Column(String, nullable=False, default=UserType.REGULAR.value)
     threads = relationship("ThreadOrm", back_populates="user")
     custom_areas = relationship("CustomAreaOrm", back_populates="user")
@@ -46,12 +46,12 @@ class ThreadOrm(Base):
     id = Column(String, primary_key=True, unique=True, nullable=False)
     user_id = Column(String, ForeignKey("users.id"), nullable=False)
     agent_id = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
     updated_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.now(),
-        onupdate=datetime.now(),
+        default=datetime.now,
+        onupdate=datetime.now,
     )
     name = Column(String, nullable=False, default="Unnamed Thread")
     is_public = Column(Boolean, nullable=False, default=False)
@@ -78,12 +78,12 @@ class RatingOrm(Base):
     trace_id = Column(String, nullable=False)
     rating = Column(Integer, nullable=False)
     comment = Column(String, nullable=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
     updated_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.now(),
-        onupdate=datetime.now(),
+        default=datetime.now,
+        onupdate=datetime.now,
     )
     user = relationship(
         "UserOrm", back_populates="ratings", foreign_keys=[user_id]
@@ -112,12 +112,12 @@ class CustomAreaOrm(Base):
     user_id = Column(String, ForeignKey("users.id"), nullable=False)
     name = Column(String, nullable=False)
     geometries = Column(JSONB, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
     updated_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.now(),
-        onupdate=datetime.now(),
+        default=datetime.now,
+        onupdate=datetime.now,
     )
 
     user = relationship("UserOrm", back_populates="custom_areas")
