38e0c92edc896fefb4333b9184c5769130913376
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-08-21 11:48:35 +0100

    Fix dist alerts

--- stac/datasets/dist_alerts.py
+++ stac/datasets/dist_alerts.py
@@ -8,8 +8,6 @@ from pystac import (
     Collection,
     Extent,
     Item,
-    Provider,
-    ProviderRole,
     SpatialExtent,
     TemporalExtent,
     set_stac_version,
@@ -18,6 +16,7 @@ from rio_stac import create_stac_item
 
 from stac.datasets.utils import (
     convert_valid_percentage_to_int,
+    get_metadata_from_yaml,
     load_stac_data_to_db,
 )
 
@@ -112,7 +111,7 @@ def create_dist_items() -> list[Item]:
         with_raster=True,
         with_proj=True,
         properties={
-            "start_datetime": str(datetime(2015, 1, 1)),
+            "start_datetime": str(datetime(2023, 1, 1)),
             "end_datetime": str(end_date),
         },
         asset_name="default",
@@ -144,69 +143,31 @@ def create_collection() -> Collection:
 
     spatial_extent = SpatialExtent(bboxes=[[-180, -60, 180, 75]])
     temporal_extent = TemporalExtent(
-        intervals=[[datetime(2015, 1, 1), end_date]]
+        intervals=[[datetime(2023, 1, 1), end_date]]
     )
     collection_extent = Extent(
         spatial=spatial_extent, temporal=temporal_extent
     )
 
+    metadata = get_metadata_from_yaml(
+        "src/tools/analytics_datasets.yml", "Ecosystem disturbance alerts"
+    )
+
     return Collection(
         id=COLLECTION_ID,
-        description="""
-            This dataset is a derivative of the OPERA's DIST-ALERT product (OPERA Land Surface
-            Disturbance Alert from Harmonized Landsat Sentinel-2 product), which is derived through
-            a time-series analysis of harmonized data from the NASA/USGS Landsat and ESA Sentinel-2
-            satellites (known as the HLS dataset). The product identifies and continuously monitors
-            vegetation cover change in 30-m pixels across the globe. It can be accessed through the
-            LPDAAC website here: https://search.earthdata.nasa.gov/search?q=C2746980408-LPCLOUD,
-            and on Google Earth Engine (GEE) with asset ID: projects/glad/HLSDIST/current
-
-            The DIST-ALERT on GFW is a derivative of this, and additional data layers not used in
-            the GFW product are available through the LPDAAC and GEE such as initial vegetation
-            fraction, and disturbance duration. While the version on the LPDAAC is updated every
-            2-4 days, the data is updated weekly on GFW. The product detects notable reductions in
-            vegetation cover (measured as "vegetation fraction" or the percent of the ground that is
-            covered by vegetation) for every pixel every time the new satellite data is acquired and
-            the ground is not obscured by clouds or snow.
-
-            The current vegetation fraction estimate is compared to the minimum fraction for the same
-            time period (within 15 days before and after) in the previous 3 years, and if there is a
-            reduction, then the system identifies an alert in that pixel. Anomalies of at least a 10%
-            reduction from the minimum become alerts in the original product, and on GFW, a higher
-            threshold of 30% is used, to reduce noise, and false alerts in the dataset. Because the
-            product compares each pixel to the minimum for the same time period in previous years, it
-            takes into account regular seasonal variation in vegetation cover. As the product is global
-            and detects vegetation anomalies, much of the data may not be applicable to GFW users
-            monitoring forests. Therefore, we mask the alerts with UMD's tree cover map, allowing users
-            to view only alerts within 30% canopy cover.
-        """,
-        title="Global all ecosystem disturbance alerts (DIST-ALERT)",
-        license="CC-BY-SA-4.0",
-        keywords=["vegetation", "disturbance", "alert", "wri"],
-        providers=[
-            Provider(
-                name="World Resources Institute",
-                roles=[ProviderRole.PRODUCER, ProviderRole.LICENSOR],
-                url="https://github.com/wri/natural-lands-map/tree/main",
-            )
-        ],
+        description=metadata.pop("description"),
+        title=metadata.pop("dataset_name"),
+        license=metadata.pop("license"),
+        keywords=metadata.pop("keywords"),
         extent=collection_extent,
-        extra_fields={
-            "default_band": {
-                "description": "(10000 * confidence) + (days since 2015)",
-            },
-            "intensity_band": {
-                "description": "this is a hotspot layer where all alerts have value 255 and bilinear resampling on the overviews so that areas with dense alerts have higher values",
-            },
-            "version": "v001",
-        },
+        extra_fields=metadata,
     )
 
 
 def main():
     items = create_dist_items()
     collection = create_collection()
-    load_stac_data_to_db(collection, items, delete_existing_items=True)
+    load_stac_data_to_db(collection, items, delete_existing_items=False)
 
 
 if __name__ == "__main__":
