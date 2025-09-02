9d63910b0c5e45d3207a596f8af902f368f478fb
Sanjay Bhangar <sanjay@developmentseed.org>
2025-09-02 18:20:11 +0530

    lintfix

--- db/alembic/versions/8f636b664598_add_has_profile_field_to_users_table.py
+++ db/alembic/versions/8f636b664598_add_has_profile_field_to_users_table.py
@@ -5,22 +5,30 @@ Revises: 95d9d8ca3bf1
 Create Date: 2025-09-02 16:57:45.873121
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '8f636b664598'
-down_revision: Union[str, None] = '95d9d8ca3bf1'
+revision: str = "8f636b664598"
+down_revision: Union[str, None] = "95d9d8ca3bf1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.add_column("users", sa.Column("has_profile", sa.Boolean(), nullable=False, server_default=sa.text("false")))
+    op.add_column(
+        "users",
+        sa.Column(
+            "has_profile",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.text("false"),
+        ),
+    )
 
 
 def downgrade() -> None:
