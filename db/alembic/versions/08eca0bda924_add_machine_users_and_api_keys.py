048a1b2f596ab972daee7d05617584cc2e2a8e39
Tarashish Mishra <sunu@sunu.in>
2025-09-03 09:22:00 +0000

    update down_revision to correct previous migration reference

--- db/alembic/versions/08eca0bda924_add_machine_users_and_api_keys.py
+++ db/alembic/versions/08eca0bda924_add_machine_users_and_api_keys.py
@@ -1,7 +1,7 @@
 """add_machine_users_and_api_keys
 
 Revision ID: 08eca0bda924
-Revises: 831ad1e71a80
+Revises: 95d9d8ca3bf1
 Create Date: 2025-09-01 09:06:34.832320
 
 """
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "08eca0bda924"
-down_revision: Union[str, None] = "831ad1e71a80"
+down_revision: Union[str, None] = "95d9d8ca3bf1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
