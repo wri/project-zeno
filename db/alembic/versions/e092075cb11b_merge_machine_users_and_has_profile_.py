80c553742b254b32c3496301759dca6cb4c6bfb1
Sanjay Bhangar <sanjay@developmentseed.org>
2025-09-03 18:03:55 +0530

    lintfixes

--- db/alembic/versions/e092075cb11b_merge_machine_users_and_has_profile_.py
+++ db/alembic/versions/e092075cb11b_merge_machine_users_and_has_profile_.py
@@ -5,15 +5,12 @@ Revises: 08eca0bda924, 8f636b664598
 Create Date: 2025-09-03 18:00:57.505369
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = 'e092075cb11b'
-down_revision: Union[str, None] = ('08eca0bda924', '8f636b664598')
+revision: str = "e092075cb11b"
+down_revision: Union[str, None] = ("08eca0bda924", "8f636b664598")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
