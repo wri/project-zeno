6e7ec7dd9adf6c39d673a121a483abbc9e1ab21d
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-11-25 10:09:58 +0000

    Remove thinking level param

--- src/utils/llms.py
+++ src/utils/llms.py
@@ -26,7 +26,6 @@ GEMINI = ChatGoogleGenerativeAI(
     max_tokens=None,  # max_tokens=None means no limit
     include_thoughts=False,
     max_retries=2,
-    thinking_level="low",
 )
 GEMINI_FLASH = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
